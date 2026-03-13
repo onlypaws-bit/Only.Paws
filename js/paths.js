118d6e0381e4628485186791cc2e81bb3db664fb
onlypaws-bit <onlypaws2026@gmail.com>
2026-03-13 17:31:55 +0100

    Update paths.js

--- js/paths.js
+++ js/paths.js
@@ -1,90 +1,68 @@
 window.OP_PATHS = {
   app: {
-    authCallback: "../app/auth-callback.html",
-    feed: "../app/feed.html",
-    post: "../app/post.html",
-    profile: "../app/profile.html"
+    authCallback: "/html/app/auth-callback.html",
+    feed: "/html/app/feed.html",
+    post: "/html/app/post.html",
+    profile: "/html/app/profile.html"
   },
 
   creators: {
-    createPost: "../creators/create-post.html",
-    creatorDash: "../creators/creator-dash.html",
-    creatorProfile: "../creators/creator-profile.html",
-    payoutsSetup: "../creators/payouts-setup.html",
-    pets: "../creators/pets.html"
+    createPost: "/html/creators/create-post.html",
+    creatorDash: "/html/creators/creator-dash.html",
+    creatorProfile: "/html/creators/creator-profile.html",
+    payoutsSetup: "/html/creators/payouts-setup.html",
+    pets: "/html/creators/pets.html"
   },
 
   fans: {
-    fanDash: "../fans/fan-dash.html",
-    fanProfile: "../fans/fan-profile.html",
-    purchasedPosts: "../fans/purchased-posts.html",
-    subscriptions: "../fans/subscriptions.html"
+    fanDash: "/html/fans/fan-dash.html",
+    fanProfile: "/html/fans/fan-profile.html",
+    purchasedPosts: "/html/fans/purchased-posts.html",
+    subscriptions: "/html/fans/subscriptions.html"
   },
 
   faq: {
-    creators: "../faq/faq-creators.html",
-    fans: "../faq/faq-fans.html"
+    creators: "/html/faq/faq-creators.html",
+    fans: "/html/faq/faq-fans.html"
   },
 
   legal: {
-    contentPolicy: "../legal/content-policy.html",
-    privacyPolicy: "../legal/privacy-policy.html",
-    stripe: "../legal/stripe.html",
-    terms: "../legal/terms.html"
+    contentPolicy: "/html/legal/content-policy.html",
+    privacyPolicy: "/html/legal/privacy-policy.html",
+    stripe: "/html/legal/stripe.html",
+    terms: "/html/legal/terms.html"
   },
 
   marketing: {
-    index: "index.html",
-    creators: "creators.html",
-    fans: "fans.html",
-    thePack: "the-pack.html",
-    emailConfirmed: "email-confirmed.html",
-    resetPassword: "reset-password.html",
-
-    indexFromOtherFolders: "../marketing/index.html",
-    creatorsFromOtherFolders: "../marketing/creators.html",
-    fansFromOtherFolders: "../marketing/fans.html",
-    thePackFromOtherFolders: "../marketing/the-pack.html",
-    emailConfirmedFromOtherFolders: "../marketing/email-confirmed.html",
-    resetPasswordFromOtherFolders: "../marketing/reset-password.html"
+    index: "/html/marketing/index.html",
+    creators: "/html/marketing/creators.html",
+    fans: "/html/marketing/fans.html",
+    thePack: "/html/marketing/the-pack.html",
+    emailConfirmed: "/html/marketing/email-confirmed.html",
+    resetPassword: "/html/marketing/reset-password.html"
   },
 
   thanks: {
-    creatorMembership: "../thanks/thanks-creator-membership.html",
-    creatorPlan: "../thanks/thanks-creator-plan.html",
-    fanMembership: "../thanks/thanks-fan-membership.html",
-    supportUs: "../thanks/thanks-support-us.html"
+    creatorMembership: "/html/thanks/thanks-creator-membership.html",
+    creatorPlan: "/html/thanks/thanks-creator-plan.html",
+    fanMembership: "/html/thanks/thanks-fan-membership.html",
+    supportUs: "/html/thanks/thanks-support-us.html"
   },
 
   assets: {
-    logo: "../../assets/images/logo.png",
-    creatorsImage: "../../assets/images/onlypaws-creators.png",
-    fansImage: "../../assets/images/onlypaws-fans.png",
-    indexImage: "../../assets/images/onlypaws-index.png",
-    pawCrown: "../../assets/images/paw-crown.png",
-    pawDiamond: "../../assets/images/paw-diamond.png",
-    pawStars: "../../assets/images/paw-stars.png"
+    logo: "/assets/images/logo.png",
+    creatorsImage: "/assets/images/onlypaws-creators.png",
+    fansImage: "/assets/images/onlypaws-fans.png",
+    indexImage: "/assets/images/onlypaws-index.png",
+    pawCrown: "/assets/images/paw-crown.png",
+    pawDiamond: "/assets/images/paw-diamond.png",
+    pawStars: "/assets/images/paw-stars.png"
   },
 
   components: {
-    header: "../../components/header.html",
-    footer: "../../components/footer.html",
-    headerMarketing: "../../components/header-marketing.html",
-    footerMarketing: "../../components/footer-marketing.html"
-  },
-
-  static: {
-    css: "../../css/styles.css",
-    authGuardJs: "../../js/auth-guard.js",
-    feedJs: "../../js/feed.js",
-    navJs: "../../js/nav.js",
-    onlypawsClientJs: "../../js/onlypawsClient.js",
-    partialsJs: "../../js/partials.js",
-    postCardJs: "../../js/post-card.js",
-    supabaseJs: "../../js/supabase.js",
-    supabaseLikesJs: "../../js/supabase-likes.js",
-    supportUsJs: "../../js/support-us.js",
-    walletJs: "../../js/wallet.js",
-    pathsJs: "../../js/paths.js"
+    header: "/components/header.html",
+    footer: "/components/footer.html",
+    headerMarketing: "/components/header-marketing.html",
+    footerMarketing: "/components/footer-marketing.html"
   }
 };
